4a1719437e2ca90134c697f7c5fc24c2d4a2f53a
Jeff Lupinski <jeff@figmints.com>
2013-09-22 07:58:41 -0400

    Update Google Analytics user code

--- themes/lupis/footer.php
+++ themes/lupis/footer.php
@@ -1,15 +1,17 @@
 			</main>
 		</div>
-		
+
 		<?php wp_footer(); ?>
-		
+
 		<?php //analytics ?>
 		<script>
-			var _gaq=[['_setAccount','UA-XXXXXXXX-XX'],['_trackPageview']];
+			var _gaq=[['_setAccount','UA-31693149-1'],['_trackPageview']];
 			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 			s.parentNode.insertBefore(g,s)})(document,'script');
 		</script>
-	
+
+	<!-- I ♥ WordPress -->
+
 	</body>
 </html>
